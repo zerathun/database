6bd6b3ac28a5b0b05546cf220a6a8f381ea5a219
zerathun <zeradun@embin.ch>
2017-12-28 15:12:43 +0100

    added funclib

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Funclib\\' => array($vendorDir . '/zerathun/funclib/src'),
+    'Database\\' => array($baseDir . '/src'),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,33 @@ namespace Composer\Autoload;
 
 class ComposerStaticInita3c1c3c070ad740fc5bcc3bad0da2af7
 {
+    public static $prefixLengthsPsr4 = array (
+        'F' => 
+        array (
+            'Funclib\\' => 8,
+        ),
+        'D' => 
+        array (
+            'Database\\' => 9,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Funclib\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/zerathun/funclib/src',
+        ),
+        'Database\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/src',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInita3c1c3c070ad740fc5bcc3bad0da2af7::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInita3c1c3c070ad740fc5bcc3bad0da2af7::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
